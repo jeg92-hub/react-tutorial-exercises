c64cb8336ab3fb9af79b16dd146bf578057f9d09
joneganagranado <joneganagranado@hotmail.com>
2021-11-01 20:46:56 +0000

    terminar practica

--- .learn/resets/05-adding-styles/app.jsx
+++ .learn/resets/05-adding-styles/app.jsx
@@ -4,9 +4,7 @@ import PropTypes from "prop-types";
 
 //add the styles here
 const mySuperStyles = {
-    fontSize: "16px",
-    border: "1px solid yellow",
-	background:"black"
+
 };
 
 const Badge = props => {
